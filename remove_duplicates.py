b9b05951a640d2926088692fe31a2c1515096169
aprashantz <prashantghi8@gmail.com>
2022-11-15 11:43:25 +0545

    add remove_duplicates question

---
+++ remove_duplicates.py
@@ -0,0 +1,11 @@
+def remove_duplicates(data):
+    p = 0
+    for i in range(1, len(data)):
+        if data[i] != data[p]:
+            p += 1
+            data[p] = data[i]
+    return len(data[0:p+1])
+
+
+# test below
+print(remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
