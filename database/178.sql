6afd89146c040d48732fd05787895071f678673d
aprashantz <prashantghi8@gmail.com>
2022-12-11 19:54:07 +0545

    177, 178, 182, 197

---
+++ database/178.sql
@@ -0,0 +1,14 @@
+/* 178. Rank Scores */
+
+/* Without using DENSE_RANK() */
+SELECT s1.score,
+(SELECT COUNT(DISTINCT s2.score) FROM Scores s2
+WHERE s2.score >= s1.score) AS 'rank'
+FROM Scores s1 ORDER BY s1.score DESC;
+
+/* Using DENSE_RANK() */
+SELECT score,
+DENSE_RANK() OVER(
+    ORDER BY score DESC
+) AS 'rank'
+FROM Scores;
---
+++ database/182.sql
@@ -0,0 +1,4 @@
+/* 182 Duplicate Emails */
+SELECT email AS Email FROM Person
+GROUP BY email
+HAVING COUNT(*) > 1;
