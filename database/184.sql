cc60e11148d69bdd31695a3f730809917aa69c99
aprashantz <prashantghi8@gmail.com>
2022-12-13 23:50:29 +0545

    adds department highest salary task database 184

---
+++ database/184.sql
@@ -0,0 +1,8 @@
+/* 184 Department Highest Salary */
+SELECT d.name AS 'Department', e.name AS 'Employee', salary AS 'Salary'
+FROM Employee e JOIN Department d
+ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) IN
+(SELECT departmentId, MAX(salary)
+FROM Employee
+GROUP BY departmentId);
