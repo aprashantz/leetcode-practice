57ad4f9cab86c8a81201ff715e465f7f3df1bba3
aprashantz <prashantghi8@gmail.com>
2022-12-14 16:17:20 +0545

    262 626 database task

---
+++ database/626.sql
@@ -0,0 +1,11 @@
+/* 626 Exchange Seats */
+SELECT (
+    CASE
+    WHEN id % 2 != 0 AND counts != id THEN id + 1
+    WHEN id % 2 != 0 AND counts = id THEN id
+    ELSE id - 1
+    END
+    ) AS 'id',
+student
+FROM Seat, (SELECT COUNT(*) AS counts FROM Seat) AS seat_counts
+ORDER BY id;
