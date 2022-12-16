2e23ec85b3783323339b48b2663f2876d387893e
aprashantz <prashantghi8@gmail.com>
2022-12-16 17:21:38 +0545

    database 1084 added, sql I completed

---
+++ database/1084.sql
@@ -0,0 +1,7 @@
+/* 1084 Sales Analysis III */
+SELECT DISTINCT p.product_id, p.product_name
+FROM Product p JOIN Sales s
+ON p.product_id = s.product_id
+WHERE s.product_id NOT IN
+(SELECT product_id FROM Sales
+WHERE sale_date > '2019-03-31' OR sale_date < '2019-01-01');
