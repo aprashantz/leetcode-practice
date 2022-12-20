cf04babe1d60a75c8df7e2fb02d661a79b7cf7ef
aprashantz <prashantghi8@gmail.com>
2022-12-20 21:58:05 +0545

    adds programming 841

---
+++ programming-py/841.py
@@ -0,0 +1,19 @@
+def can_visit_all_rooms(rooms):
+    keys_in_hand = rooms[0]
+    rooms_visited = {0}
+    while keys_in_hand:
+        # popping to remove key from hand once we use that key
+        room_to_unlock = keys_in_hand.pop()
+        if room_to_unlock not in rooms_visited:
+            rooms_visited.add(room_to_unlock)
+            # now keeping the keys in hand that we find in newly unlocked room
+            keys_in_hand += rooms[room_to_unlock]
+    return len(rooms_visited) == len(rooms)
+
+
+# test below
+print(can_visit_all_rooms([[1], [2], [3], []]))  # true
+print(can_visit_all_rooms([[1, 3], [3, 0, 1], [2], [0]]))  # false
+print(can_visit_all_rooms([[2], [], [1]]))  # true
+print(can_visit_all_rooms(
+    [[4], [3], [], [2, 5, 7], [1], [], [8, 9], [], [], [6]]))  # false
